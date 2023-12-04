66209f1a2a59e2a0258bb0172a1564afcc82a057
farellohc <152900354+farellohc@users.noreply.github.com>
2023-12-04 23:10:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const wrapper document.querySelector(".wrapper");
+
+const question document.querySelector(".question");
+
+const gif document.querySelector(".gif");
+
+const yesBtn document.querySelector(*.yes-btn"); const noßtn document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () {
+
+question. innerHTML = "Aaaaa, I like you too";
+
+"https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp"; });
+
+gif.src =
+
+noßtn.addEventListener("mouseover", () {
+
+const noßtnRect noßtn.getBoundingClientRect(); const maxx window.innerWidth noßtnRect.width;
+
+const maxy = window.innerHeight noßtnRect.height;
+
+const randomX = Math.floor(Math.random() maxx); const randomy Math.floor(Math.random() maxy);
+
+noBtn.style.left = randomX + "px";
+
+noBtn.style.top = random + "px"; D;
